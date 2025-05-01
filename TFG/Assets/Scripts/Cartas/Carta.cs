4e044d0f1b790e8441690bed10f2eec280e30d30
DanielOrenes <daniel.orenesj@gmail.com>
2025-05-01 20:50:05 +0200

    Principio de las cartas, falta card UI

---
+++ TFG/Assets/Scripts/Cartas/Carta.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Carta : MonoBehaviour
+{
+    #region Fields and Properties
+    [field: SerializeField] public ScriptableCartas InfoCarta { get;private set; }
+
+    #endregion
+
+    #region Metodos
+    
+    
+    #endregion
+}
---
+++ TFG/Assets/Scripts/ScriptableObjects/ScriptableCartas.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Cartas", menuName = "Scriptable Objects/Cartas")]
+public class ScriptableCartas : ScriptableObject
+{
+    [field: SerializeField] public string nombreCarta { get; private set; }
+    [field: SerializeField] public string descripcion { get; private set; }
+    [field: SerializeField] public int coste { get; private set; }
+    [field: SerializeField] public Sprite imagen { get; private set; }
+    [field: SerializeField] public string efecto { get; private set; }
+    [field: SerializeField] public TipoCarta tipo { get; private set; }
+    [field: SerializeField] public CartaColor colores { get; private set; }
+}
+
+public enum TipoCarta
+{
+    Ataque,
+    Habilidad,
+    Eterna,
+}
+public enum CartaColor
+{
+    Rojo,
+    Azul,
+    Amarillo,
+    Verde,
+}
+
